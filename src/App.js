843653b54f48199e79940ee4fdb71fbca0686046
sachin11336 <107074376+sachin11336@users.noreply.github.com>
2022-10-10 20:31:20 +0530

    Final Checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState , useContext} from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '../screens/home/Home';
+import Details from '../screens/details/Details';
+// import { DisplayContext } from '../screens/home/DisplayImageList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+  console.log("in App");
+  console.log(props.moviesData);
+
+  return(
+    <Router>
+        <Routes>
+            <Route path ="/"  element = {<Home />} />
+            <Route path ="/Details/:id" element = {<Details />} />
+        </Routes>
+</Router>
+)
 }
-
 export default App;
--- src/screens/MovieFilter.js
+++ src/screens/MovieFilter.js
@@ -1,164 +1,285 @@
-import React, { Component } from "react";
-import {
-  Card,
-  TextField,
-  MenuItem,
-  Select,
-  InputLabel,
-  FormControl,
-  Button,
-  Checkbox,
-} from "@mui/material";
-import { createTheme } from "@mui/material/styles";
+import * as React from 'react';
+import {Component,useState} from 'react';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import { MenuItem } from '@mui/material';
+import FormControl from '@mui/material/FormControl';
+import ListItemText from '@mui/material/ListItemText';
+import Select from '@mui/material/Select';
+import Checkbox from '@mui/material/Checkbox';
+import genres from '../../common/genres';
+import artists from '../../common/artists';
+import moviesData from '../../common/moviesData';
 
-class FilterForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        name:'',
-        genre:[],
-        artist:[],
-        releaseDateStart:'',
-        releaseDateEnd:''
-    };
-  }
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { TextField } from '@material-ui/core';
+import Button from '@mui/material/Button';
+import CardActions from '@mui/material/CardActions';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import LeftPortion from '../home/LeftPortion';
 
-  render() {
-    const { Artist, Genre,Filter} = this.props;
-    let theme = createTheme();
-    let form = {
-      color: theme.palette.primary.light,
-      margin: theme.spacing.unit,
-      minWidth: 240,
-      maxWidth: 240,
-    };
+import Card from '@mui/material/Card';
 
-    let handleChange = (event) => {
-        const tempState=this.state;
-        tempState[event.target.name]=event.target.value;
-        this.setState(tempState);
-    };
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
-    const formSubmit=(e)=>{
-      e.preventDefault();
-      Filter(this.state);
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      light: '#42a5f5',
+      main : '#1976d2'
+    },
+  },
+  spacing : 8,
+  minWidth : 240,
+  maxWidth : 240
+});
+
+
+let theme1 = createTheme({
+  palette: {
+    primary: {
+      light: '#42a5f5',
+      main : '#1976d2'
+    },
+  },
+  spacing : 8,
+  minWidth : 240,
+  maxWidth : 240
+});
+
+theme1 = createTheme(theme1,{
+    typography: {
+      body1 : {
+        color: theme1.palette.primary.light
+      }
+    },
+    spacing : 8,
+    minWidth : 240,
+    maxWidth : 240
+  });
+
+    const card = (
+        <React.Fragment>
+          <CardContent>
+           <ThemeProvider theme={theme1}>
+                <Typography variant="body1" sx={{ fontSize: 22 }} gutterBottom>
+                    FIND MOVIES BY:
+                </Typography>
+            </ThemeProvider>
+          </CardContent>
+
+        </React.Fragment>
+      );
+function BottomPortion(){
+  const[movieScreened,setMovieScreen]=useState(moviesData);
+  const[genreName,setGenreName]=useState([]);
+  const[artistName,setArtistName]=useState([]);
+  const[releaseStart,setReleaseStart]=useState("");
+  const[releaseEnd,setReleaseEnd]=useState("");
+
+  const showMovie = (e) => {
+
+    let movieName = document.getElementById("movie-name").value;
+    let genreList = genreName;
+    let artistList = artistName;
+    let releaseStartDt = releaseStart;
+    let releaseEndDt = releaseEnd;
+
+    //if movie name is given then reduce the array list of movies
+    var movieNameScreened;
+        if ((movieName != "") && (genreList.length > 0) && (artistList.length > 0) && (releaseStartDt != "") && (releaseEndDt != "")) {
+          movieNameScreened = moviesData.filter(function(movie){
+          return ((movie.title.toLowerCase() === movieName.toLowerCase()) && (genreList.some(r => movie.genres.indexOf(r) >= 0))
+          && (movie.artists.some(y => y.id.includes(artistList))) && ((new Date(movie.release_date).getTime() >= new Date(releaseStartDt).getTime()) && (new Date(movie.release_date).getTime() <= new Date(releaseEndDt).getTime())))
+          })
+        }
+        else if ((movieName != "") && (genreList.length > 0) && (artistList.length > 0)) {
+          movieNameScreened = moviesData.filter(function(movie){
+          return ((movie.title.toLowerCase() === movieName.toLowerCase()) && (genreList.some(r => movie.genres.indexOf(r) >= 0)) && (movie.artists.some(y => y.id.includes(artistList))))
+          })
+        }
+        else if ((movieName != "") && (genreList.length > 0) ){
+          movieNameScreened = moviesData.filter(function(movie){
+          return ((movie.title.toLowerCase() === movieName.toLowerCase()) && (genreList.some(r => movie.genres.indexOf(r) >= 0)))
+          })
+        }
+        else if ((movieName != "") && (artistList.length > 0)) {
+          movieNameScreened = moviesData.filter(function(movie){
+          return ((movie.title.toLowerCase() === movieName.toLowerCase()) && (movie.artists.some(y => y.id.includes(artistList))) )
+          })
+        }
+        else if ((movieName != "") && (releaseStartDt != "") && (releaseEndDt != "")) {
+          movieNameScreened = moviesData.filter(function(movie){
+          return ((movie.title.toLowerCase() === movieName.toLowerCase()) && ((new Date(movie.release_date).getTime() >= new Date(releaseStartDt).getTime()) && (new Date(movie.release_date).getTime() <= new Date(releaseEndDt).getTime())))
+          })
+        }
+        else if ((genreList.length > 0) && (artistList.length > 0) && (releaseStartDt != "") && (releaseEndDt != "")) {
+        movieNameScreened = moviesData.filter(function(movie){
+        return ((genreList.some(r => movie.genres.indexOf(r) >= 0))
+        && (moviesData.filter(x => x.artists.some(y => y.id.includes(artistList)))) && ((new Date(movie.release_date).getTime() >= new Date(releaseStartDt).getTime()) && (new Date(movie.release_date).getTime() <= new Date(releaseEndDt).getTime())))
+        })
+      }
+      else if ((genreList.length > 0) && (releaseStartDt != "") && (releaseEndDt != "")) {
+        movieNameScreened = moviesData.filter(function(movie){
+        return ((genreList.some(r => movie.genres.indexOf(r) >= 0)) && ((new Date(movie.release_date).getTime() >= new Date(releaseStartDt).getTime()) && (new Date(movie.release_date).getTime() <= new Date(releaseEndDt).getTime())))
+        })
+      }
+      else if ((artistList.length > 0) && (releaseStartDt != "") && (releaseEndDt != "")) {
+        movieNameScreened = moviesData.filter(function(movie){
+        return ((movie.artists.some(y => y.id.includes(artistList))) && ((new Date(movie.release_date).getTime() >= new Date(releaseStartDt).getTime()) && (new Date(movie.release_date).getTime() <= new Date(releaseEndDt).getTime())))
+        })
+      }
+      else if ((genreList.length > 0) && (artistList.length > 0)) {
+        movieNameScreened = moviesData.filter(function(movie){
+        return ((genreList.some(r => movie.genres.indexOf(r) >= 0)) && (movie.artists.some(y => y.id.includes(artistList))) )
+        })
+      }
+      else if ((movieName != "")){
+        movieNameScreened = moviesData.filter(function(movie){
+          return ((movie.title.toLowerCase() === movieName.toLowerCase()))
+      })
+    }
+    else if ((genreList.length > 0)) {
+      movieNameScreened = moviesData.filter(function(movie){
+      return ((genreList.some(r => movie.genres.indexOf(r) >= 0)))
+      })
+    }
+    else if ((artistList.length > 0)) {
+      movieNameScreened = moviesData.filter(x => x.artists.some(y => y.id.includes(artistList)));
+    }
+    else if ((releaseStartDt != "") && (releaseEndDt != "")) {
+      movieNameScreened = moviesData.filter(function(movie){
+      return (((new Date(movie.release_date).getTime() >= new Date(releaseStartDt).getTime()) && (new Date(movie.release_date).getTime() <= new Date(releaseEndDt).getTime())))
+      })
     }
 
-    return (
-      <Card className="right">
-        <form onSubmit={formSubmit} method="post">
-          <h3 className="findMovie" style={{ ...form }}>
-            {" "}
-            FIND MOVIES BY
-          </h3>
-          <FormControl variant="standard" sx={{ mb: 3 }} fullWidth>
-            <TextField
-              fullWidth
-              id="standard-basic"
-              label="Movie Name"
-              variant="standard"
-              name="name"
-              value={this.state.name}
-              onChange={handleChange}
-            />
-          </FormControl>
-          <FormControl fullWidth variant="standard" sx={{ mb: 3 }}>
-            <InputLabel id="demo-simple-select-standard-label">
-              Genres
-            </InputLabel>
-            <Select
-              fullWidth
-              labelId="demo-simple-select-standard-label"
-              id="demo-simple-select-standard"
-            //   onChange={handleChange}
-              label="Genres"
-              name="genre"
-            >
-              <MenuItem value="" key="None">
-                <em>None</em>
-              </MenuItem>
-              {Genre.map((genre) => {
-                return (
-                  <MenuItem
-                    control={<Checkbox />}
-                    value={genre.name}
-                    key={genre.id}
-                  >
-                    <Checkbox inputProps={{ "aria-label": "controlled" }} />
-                    {genre.name}
-                  </MenuItem>
-                );
-              })}
-            </Select>
-          </FormControl>
-          <FormControl fullWidth variant="standard" sx={{ mb: 3 }}>
-            <InputLabel id="demo-simple-select-standard-label">
-              Artists
-            </InputLabel>
-            <Select
-              fullWidth
-              labelId="demo-simple-select-standard-label"
-              id="demo-simple-select-standard"
-            //   onChange={handleChange}
-              label="Artists"
-              name="artist"
-            >
-              <MenuItem value="" key="None">
-                <em>None</em>
-              </MenuItem>
-              {Artist.map((artist) => {
-                return (
-                  <MenuItem
-                    control={<Checkbox />}
-                    value={artist.name}
-                    key={artist.id}
-                  >
-                    <Checkbox inputProps={{ "aria-label": "controlled" }} />
-                    {artist.first_name + " " + artist.last_name}
-                  </MenuItem>
-                );
-              })}
-            </Select>
-          </FormControl>
-          <FormControl variant="standard" sx={{ mb: 3 }} fullWidth>
-            <TextField
-              type="date"
-              fullWidth
-              id="standard-basic"
-              label="Release Date Start"
-              variant="standard"
-              name="releaseDateStart"
-              value={this.state.releaseDateStart}
-              onChange={handleChange}
-              InputLabelProps={{
-              shrink: true,
-              }}
-            />
-          </FormControl>
-          <FormControl variant="standard" sx={{ mb: 3 }} fullWidth>
-            <TextField
-              type="date"
-              fullWidth
-              id="standard-basic"
-              label="Release Date End"
-              variant="standard"
-              name="releaseDateEnd"
-              value={this.state.releaseDateEnd}
-              onChange={handleChange}
-              InputLabelProps={{
-              shrink: true,
-              }}
-            />
-          </FormControl>
-          <FormControl variant="standard" sx={{ mb: 3 }} fullWidth>
-            <Button variant="contained" fullWidth type="submit">
-              Apply
-            </Button>
-          </FormControl>
-        </form>
-      </Card>
-    );
+      setMovieScreen(movieNameScreened);
   }
-}
+  const handleGenreChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setGenreName(
+      typeof value === 'string' ? value.split(',') : value,
+      );
+    };
+    const handleArtistChange = (event) => {
+      const {
+        target: { value },
+      } = event;
+      setArtistName(
+        typeof value === 'string' ? value.split(',') : value,
+        );
+    };
+    
+  return (
+    <div>
+      <div>
+        <LeftPortion  moviesData={movieScreened}></LeftPortion>
+      </div>
+      <div  className="right">
+        <br></br>
+        <br></br>
+        <Card variant="outlined" sx={{ width: 300, marginLeft : "64px" }}>{card}  
+          <ThemeProvider theme={theme}>
 
-export default FilterForm;
+          <FormControl sx={{ml:3, m:2, width : 250 }}>
+            <TextField id="movie-name" label="Movie Name" />
+          </FormControl>
+          <FormControl sx={{ml: 1, mr:2, mt:2, width : 250 }}>
+              <InputLabel id="multiple-checkbox-label-genre">Genres</InputLabel>
+                   <Select 
+                      labelId="genre-multiple-checkbox-label"
+                      id="genre-multiple-checkbox"
+                      multiple
+                      value={genreName}
+                      onChange={handleGenreChange}
+                      input={<OutlinedInput label="Genres" />}
+                      renderValue={(selected) => selected.join(', ')}
+                      MenuProps={MenuProps}
+                    >
+                    {genres.map((genre) => (
+                        <MenuItem key={genre.id} value={genre.name} >
+                            <Checkbox checked={genreName.indexOf(genre.name) > -1} />
+                            <ListItemText primary={genre.name} />
+                        </MenuItem>
+                     ))}
+                  </Select>
+          </FormControl>
+          <FormControl sx={{ ml: 1, mr:2, mt:2, mb: 1, width: 250 }}>
+              <InputLabel id="artist-multiple-checkbox-label">Artists</InputLabel>
+              <Select
+                      labelId="artist-multiple-checkbox-label"
+                      id="artist-multiple-checkbox"
+                      multiple
+                      value={artistName}
+                      onChange={handleArtistChange}
+                      input={<OutlinedInput label="Artists" />}
+                      renderValue={(selected) => (selected.join(', '))}
+
+                    >
+                   {artists.map((artist) => (
+                        <MenuItem key={artist.id} value={artist.id}>
+                            <Checkbox checked={artistName.indexOf(artist.id) > -1} />
+                            <ListItemText  primary={artist.first_name+" "+artist.last_name} />
+                        </MenuItem>
+                     ))}
+              </Select>
+          </FormControl>
+          <FormControl sx={{ ml: 3, mr:2, mb:2, width: 250 ,color: 'black'}}>
+              <br></br>
+              <br></br>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                  id = "releaseStartId"
+                  inputFormat="dd-MM-yyyy"
+                  value={releaseStart|| null}
+                  label = "Release Start Date"
+                  onChange={(newValue) => {
+                        setReleaseStart(newValue);
+                  }}
+                  renderInput={(params) => <TextField {...params}  id="outlined-required" InputLabelProps={{shrink: true, }} 
+                  inputProps={{...params.inputProps,placeholder: "dd-MM-yyyy"}} />} />
+              </LocalizationProvider>
+          </FormControl>
+          <FormControl sx={{ ml: 3, mr:2, mt:2,mb:2, width: 250 }}>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                    id="releaseEndId"
+                    inputFormat="dd-MM-yyyy"
+                    value={releaseEnd || null}
+                    onChange={(newValue) => {
+                                setReleaseEnd(newValue);
+                    }}
+                    label="Release End Date"
+                    renderInput={(params) => <TextField {...params}  id="outlined-required"  InputLabelProps={{shrink: true,}} 
+                    inputProps={{...params.inputProps,placeholder: "dd-MM-yyyy"}} />} />  
+                </LocalizationProvider>
+          </FormControl>
+          <FormControl sx={{ ml:3, mt:2, mb:2, mr:2, width: 250 }}> 
+            <CardActions>
+                <Button size="large" variant="contained" id="applyButton" onClick = {showMovie}>APPLY</Button>
+            </CardActions>
+        </FormControl>
+      </ThemeProvider>
+      </Card>
+    </div>
+    </div>
+  );
+          }
+  export {BottomPortion};
